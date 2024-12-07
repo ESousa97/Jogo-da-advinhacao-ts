51b1ba328b6e8d38a57113a13195a10808a94e17
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-12-07 00:27:42 -0300

    first commit

---
+++ Desafios/Desafio-1/app.js
@@ -0,0 +1,22 @@
+let title = document.querySelector('h1');
+title.innerHTML = 'Hora do desafio';
+
+function verifyClick() {
+    console.log('O botão foi clicado');
+}
+
+function alertVerify() {
+    alert('Eu amo JS');
+}
+
+function whatIsYourCity() {
+    let cidade = prompt('Qual o nome da sua cidade?');
+    cidade = alert(`Estive em ${cidade} e lembrei de você.`);
+}
+
+function sumNumbers() {
+    let numero1 = parseFloat(prompt('Digite um número para soma.'));
+    let numero2 = parseFloat(prompt('Digite o segundo número para soma.'));
+    let resultado = numero1 + numero2;
+    alert(`${numero1} mais ${numero2} é igual a ${resultado}`); 
+}
