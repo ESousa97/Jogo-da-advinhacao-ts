69d24ff36fbc72f28d11506e9b7ad04dae14f4f3
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-12-15 19:42:18 -0300

    Realizado Desafio.
    Criado lista, com alguns indices, adicionado alguns itens através do
    push na lista criada, em seguida exibido alguns indices através do
    console.log do 0 ao ultimo.

---
+++ Desafios/Desafio-4/app.js
@@ -0,0 +1,18 @@
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = [ 'JavaScript', 'C', 'C++', 'Kotlin', 'Python' ];
+
+// 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push( 'Java', 'Ruby', 'GoLang' );
+
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaTresNomes = [ 'Assembly', 'C#', 'Cobol' ];
+console.log(listaTresNomes[0]); // Primeiro elemento
+
+// 5. Exiba no console apenas o segundo elemento.
+console.log(listaTresNomes[1]); // Segundo elemento
+
+// 6. Exiba no console apenas o último elemento.
+console.log(listaTresNomes[listaTresNomes.length - 1]); // Último elemento
